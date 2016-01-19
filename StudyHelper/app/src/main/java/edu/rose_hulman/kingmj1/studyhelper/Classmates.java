9310c5f984fb649a3f7db825720372cae88878a2
reynolpt <reynolpt@rose-hulman.edu>
2016-01-18 20:28:29 -0500

    Classmates Changes
    Created ClassmatesActivity, minor modifications to Classmates and
    ClassmatesAdapter

--- StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/Classmates.java
+++ StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/Classmates.java
@@ -17,6 +17,8 @@ public class Classmates {
         return classmateEmail;
     }
 
-
+    public String getName() {
+        return classmateName;
+    }
 
 }
--- StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/ClassmatesAdapter.java
+++ StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/ClassmatesAdapter.java
@@ -1,9 +1,12 @@
 package edu.rose_hulman.kingmj1.studyhelper;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,19 +19,21 @@ public class ClassmatesAdapter extends RecyclerView.Adapter<ClassmatesAdapter.Cl
     private RecyclerView mRecyclerView;
     private ArrayList<Classmates> mClassmates = new ArrayList<>();
 
-    public void ClassmatesAdapter(Context context, RecyclerView recyclerView) {
+    public ClassmatesAdapter(Context context, RecyclerView recyclerView) {
         mContext = context;
         mRecyclerView = recyclerView;
     }
 
     @Override
     public ClassmatesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.classmate_view, parent, false);
+        return new ClassmatesViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(ClassmatesViewHolder holder, int position) {
-
+        Classmates classmate = mClassmates.get(position);
+        holder.classmateNameView.setText(classmate.getName());
     }
 
     @Override
@@ -36,9 +41,18 @@ public class ClassmatesAdapter extends RecyclerView.Adapter<ClassmatesAdapter.Cl
         return mClassmates.size();
     }
 
-    class ClassmatesViewHolder extends RecyclerView.ViewHolder {
+    class ClassmatesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView classmateNameView;
+
         public ClassmatesViewHolder(View itemView) {
             super(itemView);
+            classmateNameView = (TextView) itemView.findViewById(R.id.classmate_name_view);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //TODO: Launch the email Classmate Dialog
         }
     }
 

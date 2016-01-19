a8ce2cdecb2c59084c098823402865828a05c9ea
reynolpt <reynolpt@rose-hulman.edu>
2016-01-18 20:52:37 -0500

    Created Work Manager Activity and xml
    Work Manager Activity created

---
+++ StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/WorkManagerActivity.java
@@ -0,0 +1,15 @@
+package edu.rose_hulman.kingmj1.studyhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_manager);
+
+
+    }
+}

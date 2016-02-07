9d4a062c45ee6b3637ff0e09ba707fbf1cabafa0
Matthew King <matthewjking94@gmail.com>
2016-02-07 17:42:16 -0500

    Added "study mode" menu item
    Just putting it in now while I was thinking about it

--- StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/WorkManagerActivity.java
+++ StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/WorkManagerActivity.java
@@ -1,9 +1,14 @@
 package edu.rose_hulman.kingmj1.studyhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class WorkManagerActivity extends AppCompatActivity implements TaskAdapter.TaskCallback {
 
@@ -13,6 +18,8 @@ public class WorkManagerActivity extends AppCompatActivity implements TaskAdapte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_manager);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.work_task_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -20,6 +27,35 @@ public class WorkManagerActivity extends AppCompatActivity implements TaskAdapte
         mTaskAdapter = new TaskAdapter(this, recyclerView, this, null);
         recyclerView.setAdapter(mTaskAdapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_work_manager, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.action_study_mode) {
+            Toast placeholder = Toast.makeText(this, "Upcoming Feature", Toast.LENGTH_SHORT);
+            placeholder.show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public void onEdit(Task task) {

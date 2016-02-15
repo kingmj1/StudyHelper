da4ca3e9bc30430dc18e0858170a3a8318332aad
Matthew King <matthewjking94@gmail.com>
2016-02-15 17:26:52 -0500

    Basic Study Mode implemented
    Study mode currently either enters priority DND mode or mutes your
    phone, depending on Android version, and also dims the screen.

--- StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/WorkManagerActivity.java
+++ StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/WorkManagerActivity.java
@@ -1,6 +1,8 @@
 package edu.rose_hulman.kingmj1.studyhelper;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,11 +10,17 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class WorkManagerActivity extends AppCompatActivity implements TaskAdapter.TaskCallback {
 
     private TaskAdapter mTaskAdapter;
+
+    private boolean isSilent;
+    private AudioManager mAudioManager;
+    private Window mWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,9 @@ public class WorkManagerActivity extends AppCompatActivity implements TaskAdapte
         setContentView(R.layout.activity_work_manager);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
+        mWindow = getWindow();
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        isSilent = !(mAudioManager.getRingerMode() == AudioManager.RINGER_MODE_NORMAL);
 
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.work_task_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -48,8 +59,23 @@ public class WorkManagerActivity extends AppCompatActivity implements TaskAdapte
         }
 
         if (id == R.id.action_study_mode) {
-            Toast placeholder = Toast.makeText(this, "Upcoming Feature", Toast.LENGTH_SHORT);
-            placeholder.show();
+//            Toast placeholder = Toast.makeText(this, "Upcoming Feature", Toast.LENGTH_SHORT);
+//            placeholder.show();
+            float brightness;
+            if(isSilent) {
+                //brightness of <0 makes it default to user preferences
+                brightness = -1.0f;
+                mAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+            } else {
+                brightness = 0.05f;
+                mAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            }
+            WindowManager.LayoutParams layoutpars = mWindow.getAttributes();
+            //Set the brightness of this window
+            layoutpars.screenBrightness = brightness;
+            //Apply attribute changes to this window
+            mWindow.setAttributes(layoutpars);
+            isSilent = !isSilent;
             return true;
         }
 

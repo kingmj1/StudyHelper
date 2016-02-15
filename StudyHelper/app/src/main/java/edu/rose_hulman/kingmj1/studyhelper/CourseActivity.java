99d372eb896ca7ef10a0a8f2018672869b90ca60
reynolpt <reynolpt@rose-hulman.edu>
2016-02-14 20:54:39 -0500

    Login
    Added a Main Activity to be able to login from or continue to Courses if
    auth is correct.  Login should work for Firebase and for Google (not
    tested for Google yet).
    username1: a@b.com       pass: 12345
    username2: b@c.com       pass:12345

--- StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/CourseActivity.java
+++ StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/CourseActivity.java
@@ -1,12 +1,15 @@
 package edu.rose_hulman.kingmj1.studyhelper;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,14 +17,35 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
-public class CourseActivity extends AppCompatActivity implements CourseAdapter.CourseCallback {
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.IOException;
+
+public class CourseActivity extends AppCompatActivity
+        implements CourseAdapter.CourseCallback {
 
     private CourseAdapter mCourseAdapter;
+    private static final String FIREBASE_REPO = "reynolpt-passwordkeeper";
+    private static final String FIREBASE_URL = "https://" + FIREBASE_REPO + ".firebaseio.com/";
+    private static final String FIREBASE = "FIREBASE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +62,12 @@ public class CourseActivity extends AppCompatActivity implements CourseAdapter.C
             }
         });
 
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.course_recycler_view);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.course_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
         mCourseAdapter = new CourseAdapter(this, this, recyclerView);
         recyclerView.setAdapter(mCourseAdapter);
+
     }
 
     @Override
@@ -67,6 +92,14 @@ public class CourseActivity extends AppCompatActivity implements CourseAdapter.C
         if (id == R.id.action_work) {
             Intent workModeIntent = new Intent(this, WorkManagerActivity.class);
             startActivity(workModeIntent);
+        }
+
+        if (id == R.id.action_logout) {
+            Firebase firebase = new Firebase(FIREBASE_URL);
+            firebase.unauth();
+            Intent mainIntent = new Intent(this, MainActivity.class);
+            startActivity(mainIntent);
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -136,4 +169,6 @@ public class CourseActivity extends AppCompatActivity implements CourseAdapter.C
     public void onCourseEdit(Course course) {
         showAddEditDialog(course);
     }
+
+
 }
---
+++ StudyHelper/app/src/main/java/edu/rose_hulman/kingmj1/studyhelper/MainActivity.java
@@ -0,0 +1,196 @@
+package edu.rose_hulman.kingmj1.studyhelper;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity
+        implements LoginActivity.OnLoginListener,
+        GoogleApiClient.OnConnectionFailedListener{
+
+    private static final int REQUEST_CODE_GOOGLE_SIGN_IN = 1;
+    private static final String TAG = "FPK";
+    private static final String FIREBASE_REPO = "reynolpt-passwordkeeper";
+    private static final String FIREBASE_URL = "https://" + FIREBASE_REPO + ".firebaseio.com/";
+    private static final String FIREBASE = "FIREBASE";
+    GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            Firebase.setAndroidContext(this);
+        }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        Firebase firebase = new Firebase(FIREBASE_URL);
+        if(firebase.getAuth() == null || isExpired(firebase.getAuth())) {
+            switchToLoginFragment();
+        } else {
+            //switchToPasswordFragment(Constants.FIREBASE_URL + "/users/" + firebase.getAuth().getUid());
+
+            showCourseActivity();
+        }
+
+
+    }
+
+    private void showCourseActivity() {
+        Intent courseIntent = new Intent(this, CourseActivity.class);
+        startActivity(courseIntent);
+    }
+
+    private void switchToLoginFragment() {
+//        Intent loginIntent = new Intent(this, LoginActivity.class);
+//        startActivity(loginIntent);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment, new LoginActivity(), "Login");
+        ft.commit();
+    }
+
+    private boolean isExpired(AuthData authData) {
+        return (System.currentTimeMillis() / 1000) >= authData.getExpires();
+    }
+
+//    @Override
+//    public void onLogout() {
+//        Firebase firebase = new Firebase(FIREBASE_URL);
+//        firebase.unauth();
+//        switchToLoginFragment();
+//    }
+
+
+    class MyAuthResultHandler implements Firebase.AuthResultHandler {
+
+        @Override
+        public void onAuthenticated(AuthData authData) {
+            //switchToPasswordFragment(FIREBASE_URL + "/users/" + authData.getUid());
+            showCourseActivity();
+        }
+
+        @Override
+        public void onAuthenticationError(FirebaseError firebaseError) {
+            showLoginError(firebaseError.getMessage());
+        }
+    }
+
+
+    @Override
+    public void onLogin(String email, String password) {
+        Firebase firebase = new Firebase(FIREBASE_URL);
+        firebase.authWithPassword(email, password, new MyAuthResultHandler());
+    }
+
+    @Override
+    public void onGoogleLogin() {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(intent, REQUEST_CODE_GOOGLE_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CODE_GOOGLE_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+                //TODO         account.getDisplayName();
+                String emailAddress = account.getEmail();
+                getGoogleOAuthToken(emailAddress);
+            }
+
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void getGoogleOAuthToken(final String emailAddress) {
+        AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
+            String errorMessage = null;
+
+            @Override
+            protected String doInBackground(Void... params) {
+                String token = null;
+                try {
+                    String scope = "oauth2:profile email";
+                    token = GoogleAuthUtil.getToken(MainActivity.this, emailAddress, scope);
+                } catch (IOException transientEx) {
+                /* Network or server error */
+                    errorMessage = "Network error: " + transientEx.getMessage();
+                } catch (UserRecoverableAuthException e) {
+                /* We probably need to ask for permissions, so start the intent if there is none pending */
+                    Intent recover = e.getIntent();
+                    startActivityForResult(recover, REQUEST_CODE_GOOGLE_SIGN_IN);
+                } catch (GoogleAuthException authEx) {
+                    errorMessage = "Error authenticating with Google: " + authEx.getMessage();
+                }
+                return token;
+            }
+            @Override
+            protected void onPostExecute(String token) {
+                Log.d("FPK", "onPostExecute");
+                //TODO
+                if(token == null) {
+                    showLoginError(errorMessage);
+                } else {
+                    onGoogleLoginWithToken(token);
+                }
+            }
+        };
+        task.execute();
+    }
+
+
+    private void onGoogleLoginWithToken(String oAuthToken) {
+        //TODO: Log user in with Google OAuth Token
+        Firebase firebase = new Firebase(FIREBASE_URL);
+        firebase.authWithOAuthToken("google", oAuthToken, new MyAuthResultHandler());
+    }
+
+    @Override
+    public void onRosefireLogin(String email, String password) {
+
+    }
+
+    private void showLoginError(String message) {
+        LoginActivity loginActivity = (LoginActivity) getSupportFragmentManager().findFragmentByTag("Login");
+        loginActivity.onLoginError(message);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+
+}
